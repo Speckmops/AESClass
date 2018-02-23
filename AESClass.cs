bdeef32be01948d395bc8a44edac6ba97a453d3d
Stefan <git@par0noid.info>
2018-02-23 18:43:24 +0100

    Initial commit

---
+++ AESClass.cs
@@ -0,0 +1,41 @@
+﻿/*
+                           __                           __     
+                         /'__`\                  __    /\ \    
+ _____      __     _ __ /\ \/\ \    ___     ___ /\_\   \_\ \   
+/\ '__`\  /'__`\  /\`'__\ \ \ \ \ /' _ `\  / __`\/\ \  /'_` \  
+\ \ \L\ \/\ \L\.\_\ \ \/ \ \ \_\ \/\ \/\ \/\ \L\ \ \ \/\ \L\ \ 
+ \ \ ,__/\ \__/.\_\\ \_\  \ \____/\ \_\ \_\ \____/\ \_\ \___,_\
+  \ \ \/  \/__/\/_/ \/_/   \/___/  \/_/\/_/\/___/  \/_/\/__,_ /
+   \ \_\                                                       
+    \/_/                                      addicted to code
+
+
+Copyright (C) 2018  Stefan 'par0noid' Zehnpfennig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace par0noid
+{
+    public class AESClass
+    {
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+﻿# Config
+AESClass is a lightweight class for encrypting and decrypting with AES
+
+You can encrypt or decrypt byte[], base64-strings or normal strings to your favorite output type. 
+
+**Hint:** I'm using elements of C# 6.0. Be sure your compiler is configured for C# 6.0
+
+Dev-Website: http://par0noid.info
